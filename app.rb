30218026bda5be64e4cfe1e4b5ac1ac8959d2c60
Yosuke Mori <77700130+gyvm@users.noreply.github.com>
2024-06-12 03:38:22 +0900

    ブラウザで入力した文章をclaudeへリクエストできるようにした

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'haml'
 require_relative 'diff_html_formatter'
+require_relative 'bedrock'
 
 get '/' do
   haml :index
@@ -9,8 +10,14 @@ end
 
 post '/compare' do
   original = session[:original] = params[:original]
-  modified = session[:modified] = params[:modified]
+
+  service = ClaudeService.new
+  modified = session[:modified] = service.generate_response(create_pronpt(original))
 
   @result = DiffHTMLFormatter.new(original, modified).to_html
   haml :index
+end
+
+def create_pronpt(original)
+  "\"#{original}\"という文章を正しい日本語に校正して下さい。なるべく元の文の意味を損なわないようにしてください。"
 end
--- bedrock.rb
+++ bedrock.rb
@@ -1,34 +1,35 @@
+# frozen_string_literal: true
+
 require 'aws-sdk-bedrockruntime'
 require 'json'
 
-# Clientを作成
-client = Aws::BedrockRuntime::Client.new(region: 'us-east-1')
+class ClaudeService
+  def initialize
+    @client = Aws::BedrockRuntime::Client.new(region: 'us-east-1')
+    @model_id = "anthropic.claude-3-haiku-20240307-v1:0"
+  end
 
-# モデルとプロンプトを設定
-model_id = "anthropic.claude-3-haiku-20240307-v1:0"
-prompt = "Describe the purpose of a 'hello world' program in one line."
-
-# リクエストを作成
-native_request = {
-  anthropic_version: "bedrock-2023-05-31",
-  max_tokens: 512,
-  temperature: 0.5,
-  messages: [
-    {
-      role: "user",
-      content: [{ type: "text", text: prompt }]
+  def generate_response(prompt)
+    native_request = {
+      anthropic_version: "bedrock-2023-05-31",
+      max_tokens: 512,
+      temperature: 0.5,
+      messages: [
+        {
+          role: "user",
+          content: [{ type: "text", text: prompt }]
+        }
+      ]
     }
-  ]
-}
 
-# リクエストを送信
-response = client.invoke_model({
-  model_id: model_id,
-  body: native_request.to_json
-})
+    response = @client.invoke_model({
+      model_id: @model_id,
+      body: native_request.to_json
+    })
 
-# レスポンスを処理
-model_response = JSON.parse(response.body.read) 
-response_text = model_response["content"][0]["text"]
+    model_response = JSON.parse(response.body.read) 
+    response_text = model_response["content"][0]["text"]
 
-puts response_text
+    response_text
+  end
+end
